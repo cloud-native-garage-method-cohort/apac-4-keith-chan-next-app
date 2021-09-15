e752348489a60196531bec43f413cf5fe28773d0
Keith Chan <tze.keat.chan@ibm.com>
2021-09-15 13:49:26 +0800

    Trigger pipeline again

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe and updated GitOps Repo' })
+  res.status(200).json({ name: 'Hello! I\'ve updated GitOps Repo' })
 }
